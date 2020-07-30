ba48d51645a65964fbcd1fa716df65a2d73b27d0
nguyendm77 <nguyendm77@gmail.com>
2020-07-29 17:10:46 -0700

    inital update

--- src/components/About.js
+++ src/components/About.js
@@ -4,22 +4,22 @@ export default function About() {
     return(
         <div>
             <h1>About The Dev Team</h1>
-            <div>
+            <div class="frontend">
                 <h3>Josh Danao</h3>
                 <img src="https://ca.slack-edge.com/T0351JZQ0-U011SC6GRMK-b7744c409e1e-512" />
                 <p>Frontend development</p>
             </div>
-            <div>
+            <div class="backend">
                 <h3>Minh Nguyen</h3>
                 <img src="https://ca.slack-edge.com/T0351JZQ0-U013SS45XPW-6b458202f6f4-512" />
                 <p>Backend development</p>
             </div>
-            <div>
+            <div class="frontend">
                 <h3>Nick Hvattum</h3>
                 <img src="https://ca.slack-edge.com/T0351JZQ0-U0141JELV4Y-710a221b9c63-512" />
                 <p>Frontend development</p>
             </div>
-            <div>
+            <div class="backend">
                 <h3>Zack Barovsky</h3>
                 <img src="https://ca.slack-edge.com/T0351JZQ0-UV0CS3TQF-6394ecdb9322-512" />
                 <p>Project Manager, Git Master, Backend developer</p>
